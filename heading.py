ed6f3b6bccae771a18d387c209374ffcaf70c6e4
Chivier <chivier.humber@gmail.com>
2019-11-25 16:42:52 +0800

    Adding some comments

--- heading.py
+++ heading.py
@@ -13,6 +13,12 @@ HEAD_NOTE_BEGIN = "<font size=\"%d\"><p style = \"text-align: left;\" > "
 HEAD_NOTE_END = "</p>\n</font>"
 
 
+# * heading class
+# - includes:
+# - title information
+# - depth information (title level)
+# - subheading infoemation
+# - chapter codes here
 class Headings_:
     def __init__(self):
         title = []
@@ -23,12 +29,15 @@ class Headings_:
 
 
 def readmarkdown():
+    # * read markdown file
     with open('data.txt', encoding='utf-8') as json_file:
         parser = json.load(json_file)
     return parser
 
 
 def build_pureparagraph(js_list: List[dict]) -> str:
+    # * get a pure text for a certain paragraph
+    # - Used to generate title on the left heading
     str = ''
     for item in js_list:
         if item['type'] == 'text':
@@ -52,6 +61,7 @@ def build_pureparagraph(js_list: List[dict]) -> str:
 
 
 def build_paragraph(js_list: List[dict]) -> str:
+    # * build paragraph with different style/fonts
     str = ''
     for item in js_list:
         if item['type'] == 'text':
@@ -74,13 +84,16 @@ def build_paragraph(js_list: List[dict]) -> str:
     return str
 
 
+# todo: convert file location to a local one
 def get_link(s: str):
     return s
 
 
 def build_headings(js: List[dict], depth: int):
+    # * build heading-graph
+    # show the dependencies information and such thing
     sub_headings = []
-    # 寻找所有子标题
+    # look for all subtitles
     index = [i for i, item in enumerate(js)
              if item['type'] == 'heading' and item['depth'] == depth]
     if len(index) == 0 and len(js) == 0:
@@ -101,6 +114,7 @@ def build_headings(js: List[dict], depth: int):
 
 
 def make_index(f, headinglist):
+    # * print index_page
     subs = headinglist.sub_headings
     f.write("#"*2 + " ")
     f.write(headinglist.title+"\n")
@@ -128,14 +142,15 @@ def print_markdown(f, headinglist, indexstr=""):
             f.write(SLIDE_SINGLE_END)
             f.write(SLIDE_TOPIC_END)
 
-        f.write(SLIDE_TOPIC_BEGIN)
-        f.write(HEAD_NOTE_BEGIN % (24 - t.depth*2))
-        f.write(indexstr + str(id_count) + "  " + title_show)
-        f.write(HEAD_NOTE_END)
-        f.write(SLIDE_SINGLE_BEGIN)
-        print_content(f, t.chapter_content)
-        f.write(SLIDE_SINGLE_END+"\n")
-        f.write(SLIDE_TOPIC_END)
+        if len(t.chapter_content) != 0:
+            f.write(SLIDE_TOPIC_BEGIN)
+            f.write(HEAD_NOTE_BEGIN % (24 - t.depth*2))
+            f.write(indexstr + str(id_count) + "  " + title_show)
+            f.write(HEAD_NOTE_END)
+            f.write(SLIDE_SINGLE_BEGIN)
+            print_content(f, t.chapter_content)
+            f.write(SLIDE_SINGLE_END+"\n")
+            f.write(SLIDE_TOPIC_END)
 
         next_indexstr = indexstr + str(id_count) + "."
         print_markdown(f, t.sub_headings, next_indexstr)
@@ -144,6 +159,7 @@ def print_markdown(f, headinglist, indexstr=""):
 
 
 def print_reveal_head():
+    # * print reveal.js heading information
     head_file = open("reveal_head.txt", "r")
     head_str = head_file.read()
     fileout = open("index.html", "w")
@@ -154,9 +170,11 @@ def print_reveal_head():
 
 
 def print_reveal_tail():
+    # * print reveal.js tailing information
     tail_file = open("reveal_tail.txt", "r")
     tail_str = tail_file.read()
     fileout = open("index.html", "a")
+    # move cursor to the end
     fileout.seek(0, 2)
     fileout.write(tail_str)
     tail_file.close()
